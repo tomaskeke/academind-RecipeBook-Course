2c66bdb74978f5fb126e688baaa86af29174e415
Tomas <tonken.ek@gmail.com>
2021-12-06 13:11:59 +0100

    first push

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  constructor(private DataStorageService: DataStorageService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSaveData() {
+    this.DataStorageService.storeRecipes();
+  }
+  onFetchData() {
+    this.DataStorageService.fetchRecipes().subscribe();
+  }
+}
